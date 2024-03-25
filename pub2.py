6022dccf665be98706a57cd3ea4a89d4219251df
priyanka-kumari26 <swetapriya2612@gmail.com>
2024-03-25 21:37:58 +0530

    comminting changes

---
+++ pub2.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as paho
+import sys
+
+# Callback function on connection to broker
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker")
+    else:
+        print("Connection failed with code", rc)
+
+
+publisher = paho.Client(paho.CallbackAPIVersion.VERSION2)
+
+if publisher.connect("localhost", 1883, 60) != 0:
+    print("Could not connect to MQTT Broker\n")
+    sys.exit(-1)
+else:
+    print("Connected to MQTT Broker\n")
+
+msg = input("Enter the message you want to publish:\n")
+publisher.publish("test/status/new", msg, 0)  # nested msg topic
+
+publisher.disconnect()
---
+++ sub.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as paho
+import sys
+
+# Callback function on connection to broker
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker")
+    else:
+        print("Connection failed with code", rc)
+
+# function to print the message the publisher message on the terminal
+
+
+def onMessage(client, userdata, msg):
+    print(msg.topic + ":" + msg.payload.decode())
+
+
+client = paho.Client(paho.CallbackAPIVersion.VERSION2)
+client.on_message = onMessage
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Could not connect to MQTT Broker\n")
+    sys.exit(-1)
+else:
+    print("Connected to MQTT Broker\n")
+
+sub = input("Enter the topic you want to subscribe to:\n")
+client.subscribe(sub)
+
+try:
+    print("Press Ctrl+C to exit\n")
+    client.loop_forever()
+except:
+    print("Disconnecting from Broker\n")
+
+client.disconnect()
